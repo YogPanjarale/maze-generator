5f923da28951ef00f9a95b70bdb8e7a89d947d03
YogPanjarale <yog.panjarale@gmail.com>
2021-05-07 10:56:46 +0530

    proper grid with lines

--- sketch/cell.ts
+++ sketch/cell.ts
@@ -1,15 +1,35 @@
+type Walls={
+    top:boolean
+    left:boolean
+    bottom:boolean
+    right:boolean
+}
+
 class Cell {
-    i: number;
-    j:number;
-    constructor(i:number,j:number) {
-        this.i=i
-        this.j=j
-    }
-    show(){
-        var x = this.i*w;
-        var y = this.j*w;
-        stroke(255)
-        noFill()
-        rect(x,y,w,w)
-    }
+	i: number;
+	j: number;
+    wallsB:boolean[]=[]
+    walls:Walls={top:false,bottom:false,left:true,right:true}
+	constructor(i: number, j: number) {
+		this.i = i;
+		this.j = j;
+	}
+	show() {
+		var x = this.i * w;
+		var y = this.j * w;
+		stroke(255);
+		const _ = 0;
+        const {top,left,bottom,right}= this.walls
+        if (top)
+		    line(x + _, y + _, x + w, y + _);//top
+        if (left)
+		    line(x + w, y + _, x + w, y + w);//left
+        if (bottom)
+            line(x + w, y + w, x + _, y + w);//bottom
+		if (right)
+            line(x + _, y + w, x + _, y + _);//right
+
+		// noFill()
+		// rect(x,y,w,w)
+	}
 }
