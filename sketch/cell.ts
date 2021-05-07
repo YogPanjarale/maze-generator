32e9161dbf978aec6aa00aadaf6ea88ac5c4b4ac
YogPanjarale <yog.panjarale@gmail.com>
2021-05-07 11:05:53 +0530

    formatted

--- sketch/cell.ts
+++ sketch/cell.ts
@@ -1,15 +1,15 @@
-type Walls={
-    top:boolean
-    left:boolean
-    bottom:boolean
-    right:boolean
-}
+type Walls = {
+	top: boolean;
+	left: boolean;
+	bottom: boolean;
+	right: boolean;
+};
 
 class Cell {
 	i: number;
 	j: number;
-    wallsB:boolean[]=[]
-    walls:Walls={top:false,bottom:false,left:true,right:true}
+	wallsB: boolean[] = [];
+	walls: Walls = { top: false, bottom: false, left: true, right: true };
 	constructor(i: number, j: number) {
 		this.i = i;
 		this.j = j;
@@ -19,15 +19,11 @@ class Cell {
 		var y = this.j * w;
 		stroke(255);
 		const _ = 0;
-        const {top,left,bottom,right}= this.walls
-        if (top)
-		    line(x + _, y + _, x + w, y + _);//top
-        if (left)
-		    line(x + w, y + _, x + w, y + w);//left
-        if (bottom)
-            line(x + w, y + w, x + _, y + w);//bottom
-		if (right)
-            line(x + _, y + w, x + _, y + _);//right
+		const { top, left, bottom, right } = this.walls;
+		if (top) line(x + _, y + _, x + w, y + _); //top
+		if (left) line(x + w, y + _, x + w, y + w); //left
+		if (bottom) line(x + w, y + w, x + _, y + w); //bottom
+		if (right) line(x + _, y + w, x + _, y + _); //right
 
 		// noFill()
 		// rect(x,y,w,w)
--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -1,23 +1,23 @@
-
-var cols:number,rows:number
-var w:number=50
-var grid : Cell[]=[]
+var cols: number, rows: number;
+var w: number = 50;
+var grid: Cell[] = [];
+var current: Cell;
 function setup() {
-  console.log("🚀 - Setup initialized - P5 is running");
-  createCanvas(500, 500);
-  cols=floor(width/w)
-  rows=floor(height/w)
-  for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < cols; j++) {
-      var cell:Cell = new Cell(i,j)
-      grid.push(cell)
-    }
-  }
+	console.log("🚀 - Setup initialized - P5 is running");
+	createCanvas(500, 500);
+	cols = floor(width / w);
+	rows = floor(height / w);
+	for (let i = 0; i < rows; i++) {
+		for (let j = 0; j < cols; j++) {
+			var cell: Cell = new Cell(i, j);
+			grid.push(cell);
+		}
+	}
 }
 
 function draw() {
-  background(51);
-  grid.forEach(cell=>{
-    cell.show()
-  })
+	background(51);
+	grid.forEach((cell) => {
+		cell.show();
+	});
 }
